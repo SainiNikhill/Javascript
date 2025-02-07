d689f8dac0660a24a5d6943cfa15e2caf0c13ff1
Nikhil <saininikhil2911@gmail.com>
2025-02-07 11:50:27 +0000

    conversion of datatypes

---
+++ 1basics/conversionOperation.js
@@ -0,0 +1,38 @@
+let score="nikhil"
+
+// let valueInNumber= Number(score)
+// console.log(typeof score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)// gonna show NaN means not a number
+
+
+// "33"=> 33
+// '33av"=>NaN
+//true=>1
+//false=>0
+
+
+// boolean conversion
+let isLoggedin = 0
+
+let booleanisLoggedin=Boolean(isLoggedin)// converting number into boolean
+
+console.log(booleanisLoggedin)
+
+let string =""
+
+let booleanString=Boolean(string)// empty string converted into true and normal string into true
+
+console.log(booleanString)
+
+
+
+
+//string conversion
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log( typeof stringNumber)// 33 gets converted into string("33");
+
+
+
