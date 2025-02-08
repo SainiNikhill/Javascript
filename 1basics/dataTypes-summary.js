7e2a756cfd4a2e7f320e4f711162d2bb1a388881
Nikhil <saininikhil2911@gmail.com>
2025-02-08 12:47:08 +0000

    datatypesSummary

---
+++ 1basics/dataTypes-summary.js
@@ -0,0 +1,7 @@
+// two data types in js 
+// 1 . primitive () call by value ( copy of value is provied. if changes in copy happens ,stays in copy itself )
+// 7 primitives=> String , Number ,boolean, Null, Undefined, symbol(used to make any value unique), BigInt(larger number than the range of number)
+
+// 2. non-primitive( reference types)
+// Arrays , objects , function
+
